ac1e4b8d5c3cc95e6e8b11122d6a06eb5d16dd15
vatrostambuk <letx43@outlook.com>
2018-07-07 08:53:03 +0200

    Init project

---
+++ PlutoRover/PlutoRover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlutoRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
